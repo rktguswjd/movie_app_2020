1c5adfb0e8b3d88b60aefe0f7eec6c8f07e66a3b
rktguswjd <010_cm@naver.com>
2020-09-27 21:24:50 +0900

    Building the Navigation

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Movie from "../components/Movie";
-import "../Home.css";
+import "./Home.css";
 
 class Home extends Component {
   state = {
